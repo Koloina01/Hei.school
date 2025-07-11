3f91ae01c9c50820e01290cfa9637ec828aac0ba
Koloina01 <kolomaherison@gmail.com>
2025-07-11 20:10:41 +0300

    fix: re-install tailwind/vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import './App.css';
 import Navbar from './Components/Navbar';
 
 function App() {
@@ -12,4 +11,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
