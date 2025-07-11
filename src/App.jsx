1ebd2a623fe1dbbdafe748e538a6172f92389236
Koloina01 <kolomaherison@gmail.com>
2025-07-12 01:16:26 +0300

    feat: make the admission section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Navbar from './Components/Navbar';
+import Etudes from './Components/Etudes';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Etudes />
     </>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,19 +4,19 @@ import "../Styles/Navbar.css"
 
 export default function Navbar() {
     return (
-        <nav className="bg-blue-900 text-white">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex items-center justify-between h-16 navbar-big-container">
+        <nav className="fixed top-0 bg-[#001948] text-white">
+            <div className="w-[116rem] h-30 mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex items-center justify-around navbar-big-container">
                     <div className="navbar-logo">
-                        <img src={logo} alt="HEI Logo" className="w-10 h-6 logo-hei"/>
+                        <img src={logo} alt="HEI Logo" className="w-[6rem] h-auto logo-hei"/>
                     </div>
                     <div className="flex items-center navbar-container">  
                         <div className="hidden md:block navbar-menu">
                             <div className="ml-10 flex items-baseline space-x-4 navbar-link">
-                                <a href="#" className="hover:bg-blue-800 px-3 py-2 rounded-md text-sm font-medium navbar-navlink">ACCUEIL</a>
-                                <a href="#" className="hover:bg-blue-800 px-3 py-2 rounded-md text-sm font-medium navbar-navlink">ACTUALITÉS</a>
-                                <a href="#" className="hover:bg-blue-800 px-3 py-2 rounded-md text-sm font-medium navbar-navlink">BOURSES D'ÉTUDES</a>
-                                <a href="#" className="hover:bg-blue-800 px-3 py-2 rounded-md text-sm font-medium navbar-navlink">INSCRIPTION</a>
+                                <a href="#" className="text-[1.1rem] px-3 py-2 rounded-md text-sm font-medium navbar-navlink">ACCUEIL</a>
+                                <a href="#" className="text-[1.1rem] px-3 py-2 rounded-md text-sm font-medium navbar-navlink">ACTUALITÉS</a>
+                                <a href="#" className="text-[1.1rem] px-3 py-2 rounded-md text-sm font-medium navbar-navlink">BOURSES D'ÉTUDES</a>
+                                <a href="#" className="text-[1.1rem] px-3 py-2 rounded-md text-sm font-medium navbar-navlink">INSCRIPTION</a>
                             </div>
                         </div>
                         <div className="hidden md:block navbar-button">
