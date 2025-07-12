563beb9244ed12265767570ddd741b988f4b55eb
harensoa259 <narovanaharentsoa@gmail.com>
2025-07-12 12:09:06 +0300

    Implementinng App.jsx after importion First_section.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Navbar from './Components/Navbar';
 import Etudes from './Components/Etudes';
+import First_section from './Components/First_section';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <First_section />
       <Etudes />
     </>
   )
